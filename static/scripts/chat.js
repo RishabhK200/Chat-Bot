05a07a17c0317d4f64be5323d5beec9dd2377042
Rishabh Kanth <rishabhkanth200@gmail.com>
2023-07-18 09:53:38 +0530

    done

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,29 +1,82 @@
-// Variable to store the user's selected option
-let selectedOption = "";
 
-// Function to handle the user's option selection
-function handleOptionSelection(option) {
-  selectedOption = option;
+// Function for expanding 
 
-  let userHtml = '<p class="userText"><span>' + option + '</span></p>';
-  $("#chatbox").append(userHtml);
-  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+var coll = document.getElementsByClassName("collapsible");
 
-  setTimeout(() => {
-    getHardResponse(selectedOption);
-  }, 1000);
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+    });
 }
+
+function getTime() {
+    let today = new Date();
+    hours = today.getHours();
+    minutes = today.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let time = hours + ":" + minutes;
+    return time;
+}
+
+let N_message = ""
+
+// Gets the first message
+function firstBotMessage() {
+    let firstMessage = "Hi, how may I help you?"
+    let secondMessage = "Select the number from below"
+    let thirdMessage_1 = "1) Course registration and Enrollment "
+    let thirdMessage_2 = "2) Campus facilities and Services "
+    let thirdMessage_3 = "3) Class Schedules and Attendance Policies"
+    let thirdMessage_4 = "4) Academic deadlines and Important Dates "
+    let thirdMessage_5 = "5) Student Code of Conduct and Disciplinary Policies "
+
+    let firstBotMessageHTML = '<p class="botText"><span>' + firstMessage +  '</span></p>';
+    let secondBotMessageHTML = '<p class="botText"><span>' + secondMessage +  '</span></p>';
+    let thirdBotMessageHTML = '<p class="botText"><span>' + thirdMessage_1 + '</br>' + thirdMessage_2 +'</br>' + thirdMessage_3 +'</br>'+ thirdMessage_4 +'</br>' + thirdMessage_5+'</span></p>';
+
+
+    document.getElementById("botStarterMessage").innerHTML = firstBotMessageHTML + secondBotMessageHTML + thirdBotMessageHTML ;
+
+    let time = getTime();
+
+    $("#chat-timestamp").append(time);
+    document.getElementById("userInput").scrollIntoView(false);
+
+    
+}
+
+ firstBotMessage() ;
 
 // Retrieves the response
 function getHardResponse(userText) {
-  let botResponse = "";
+
+  let botResponse = userText;
+  let botResponseans = "";
   let botResponse1 = "";
+  let botResponseans1 = "";
 
   if (userText === "1") {
     botResponse = "1) How do I register for courses?";
     botResponseans = "Course registration is typically done through the college's online student portal. You will need to log in with your student credentials and follow the instructions provided for course selection and registration." ;
     botResponse1 = "2) How many courses can someone register per semester?";
-    botResponseans2 = "The number of courses you can register for per semester may vary depending on factors such as your academic program, credit load requirements, and any specific guidelines or policies set by the college. It is advisable to consult with your academic advisor or refer to the college's course catalog for the recommended course load." ;
+    botResponseans1 = "The number of courses you can register for per semester may vary depending on factors such as your academic program, credit load requirements, and any specific guidelines or policies set by the college. It is advisable to consult with your academic advisor or refer to the college's course catalog for the recommended course load." ;
   }
   
   else if (userText === "2") {
@@ -54,136 +107,58 @@ function getHardResponse(userText) {
     botResponseans1 =  "The student code of conduct is typically available on the college's website or through the student affairs office. It is important to review and familiarize yourself with the code to understand the expectations and consequences associated with student behavior.";
   }
 
-  let botHtml = '<p class="botText"><span>' + botResponse + '</br>' + botResponseans +  '</br>' + botResponse1 + '</br>' + botResponseans1 + '</span></p>';
-  $("#chatbox").append(botHtml);
 
-  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+    // let botResponse = getBotResponse(userText);
+    let botHtml = '<p class="botText"><span>' + botResponse + '</br>' + botResponseans + '</br>' + botResponse1 + '</br>' + botResponseans1 + '</span></p>';
+    $("#chatbox").append(botHtml);
 
-  // Clear the selected option for the next iteration
-  selectedOption = "";
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
 
-// Function to handle the user's text input
+//Gets the text text from the input box and processes it
 function getResponse() {
-  let userText = $("#textInput").val();
+    let userText = $("#textInput").val();
+    // let output = " ";
 
-  if (userText === "") {
-    userText = "Invalid, enter value";
-  }
-
-  let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
-
-  $("#textInput").val("");
-  $("#chatbox").append(userHtml);
-  document.getElementById("chat-bar-bottom").scrollIntoView(true);
-
-  // If the user has selected an option, handle the option selection
-  if (selectedOption) {
-    setTimeout(() => {
-      handleOptionSelection(selectedOption);
-    }, 1000);
-  } else {
-    setTimeout(() => {
-      getHardResponse(userText);
-    }, 1000);
-  }
-}
-
-// Function for expanding
-var coll = document.getElementsByClassName("collapsible");
-
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-
-    var content = this.nextElementSibling;
-
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+    if (userText == "") {
+        userText = "Invalid, enter something";
     }
-  });
+    
+
+    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    setTimeout(() => {
+        getHardResponse(userText);
+    }, 1000)
+
 }
-
-function getTime() {
-  let today = new Date();
-  let hours = today.getHours();
-  let minutes = today.getMinutes();
-
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  let time = hours + ":" + minutes;
-  return time;
-}
-
-let N_message = "";
-
-// Gets the first message
-function firstBotMessage() {
-  let firstMessage = "Hi, how may I help you?";
-  let secondMessage = "Select the number from below";
-  let thirdMessage_1 = "1) Course registration and Enrollment";
-  let thirdMessage_2 = "2) Campus facilities and Services";
-  let thirdMessage_3 = "3) Class Schedules and Attendance Policies";
-  let thirdMessage_4 = "4) Academic deadlines and Important Dates";
-  let thirdMessage_5 = "5) Student Code of Conduct and Disciplinary Policies";
-
-  let firstBotMessageHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
-  let secondBotMessageHTML = '<p class="botText"><span>' + secondMessage + '</span></p>';
-  let thirdBotMessageHTML =
-    '<p class="botText"><span>' +
-    thirdMessage_1 +
-    '</br>' +
-    thirdMessage_2 +
-    '</br>' +
-    thirdMessage_3 +
-    '</br>' +
-    thirdMessage_4 +
-    '</br>' +
-    thirdMessage_5 +
-    "</span></p>";
-
-  document.getElementById("botStarterMessage").innerHTML = firstBotMessageHTML + secondBotMessageHTML + thirdBotMessageHTML;
-
-  let time = getTime();
-
-  $("#chat-timestamp").append(time);
-  document.getElementById("userInput").scrollIntoView(false);
-}
-
-firstBotMessage();
 
 // Handles sending text via button clicks
 function buttonSendText(sampleText) {
-  let userHtml = '<p class="userText"><span>' + sampleText + "</span></p>";
+    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
 
-  $("#textInput").val("");
-  $("#chatbox").append(userHtml);
-  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-  // Handle the button response based on the sampleText
-  handleOptionSelection(sampleText);
+    
+}
+
+function sendButton() {
+    getResponse();
+}
+
+function heartButton() {
+    buttonSendText("Heart clicked!")
 }
 
 // Press enter to send a message
 $("#textInput").keypress(function (e) {
-  if (e.which == 13) {
-    getResponse();
-  }
-});
-
-// Button click event handlers
-function sendButton() {
-  getResponse();
-}
-
-function heartButton() {
-  buttonSendText("Heart clicked!");
-}
+    if (e.which == 13) {
+        getResponse();
+    }
+})
--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -1,38 +1,12 @@
 function getBotResponse(input) {
-    //user inputs 
 
-    if (input === "rock") {
-        return " "; 
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
-    }
-
-    // Simple responses
     if (input == "hello") {
         return "Hello there!";
     } else if (input == "goodbye") {
         return "Talk to you later!";
-    } else {
+    } 
+    else {
         return "Try asking something else!";
     }
 }
 
-
-// if (userText == "1") { 
-//     userText = "1) How do I register for courses? ";
-//     userText1 = "2) How many courses can someone register per semester? " ;
-// }
-// else if (userText == "2") { 
-//     userText = "1) How many courses can someone register per semester? ";
-// }
-// else if (userText == "1") { 
-//     userText = "1) How many courses can someone register per semester? "
-// }
-// else if (userText == "1") { 
-//     userText = "1) How many courses can someone register per semester? "
-// }
-// if (userText == "1") { 
-//     userText = "1) How many courses can someone register per semester? "
-// }
